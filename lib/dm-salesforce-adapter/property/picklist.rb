15e61c3e18476dad309708848d6411ee023ce0d1
Frederic Jean <fred@fredjean.net>
2012-04-13 11:18:59 -0600

    Dealing with all kinds of picklists...

--- lib/dm-salesforce-adapter/property/picklist.rb
+++ lib/dm-salesforce-adapter/property/picklist.rb
@@ -2,7 +2,7 @@ module SalesforceAdapter::Property
   class Picklist < ::DataMapper::Property::String
     def values_for_picklist
       @metadata ||= adapter.metadata(model)
-      @values ||= @metadata.fields.select { |f| f.type == "picklist" && (f.name =~ /#{self.field}/i || f.name =~ /#{self.name.to_s}/i)}.first.picklistValues rescue []
+      @values ||= @metadata.fields.select { |f| f.type =~ /picklist$/ && (f.name =~ /#{self.field}/i || f.name =~ /#{self.name.to_s}/i)}.first.picklistValues rescue []
       @values.map {|value| {:value => value.value, :label => value.label}}
     end
 
